f8c7cfa04a106413080bc507fced03b1907504ab
Panjala-Thilak <54017470+Panjala-Thilak@users.noreply.github.com>
2019-09-15 17:28:31 +0530

    Add files via upload

---
+++ Stacks/minofstack.c
@@ -0,0 +1,89 @@
+//Minimum element in the stack
+#include<stdio.h>
+#define max 10
+int stack[max];
+int minstack[max];
+int top=-1;
+int top1=-1;
+void display();
+int min;
+int pop();
+void push(int );
+void push1(int );
+int pop1();
+int main()
+{
+	int ch,x,c,n;
+	do
+	{
+		printf("enter ur choice 1.push 2.pop");
+		scanf("%d",&ch);
+		switch(ch)
+		{
+			case 1:if(top==max-1)
+					printf("stack overflow");
+					else
+					{
+					printf("enter the element to be inserted ");
+					scanf("%d",&x);
+					if(top1==-1)
+					{
+						min=x;
+					}
+					if(min>x)
+					push1(x);
+					else
+					push1(min);
+					push(x);
+					printf("the elements in the stack are:\n ");
+					display();
+					printf("the minimum element in the stack is %d",minstack[top]);
+				}
+				break;
+			case 2:if(top==-1)
+					printf("stack underflow ");
+					else
+					{
+						n=pop();
+						pop1();
+						printf("the poped element is %d \n",n);
+							printf("the elements in the stack are:\n");
+						display();
+						printf("the minimum element in the stack is %d",minstack[top]);
+					}
+					break;
+			default:printf("enter the valid choice ");
+
+					
+		}
+		printf("\n enter 1 to continue");
+		scanf("%d",&c);
+	}while(c==1);
+	
+	return 1;
+}
+void push(int x)
+{
+	stack[++top]=x;
+}
+int pop()
+{
+	return stack[top--];
+}
+void display()
+{
+	int i;
+	for(i=top;i>=0;i--)
+	{
+		printf("%d\n",stack[i]);
+	}
+}
+void push1(int x)
+{
+	minstack[++top1]=x;
+}
+int pop1()
+{
+	return minstack[top1--];
+}
+
